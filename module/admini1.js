d221c0f0468ac88d56771925dca47cc3f4eee1b8
Unknown <babathiste@gmail.com>
2017-09-16 00:56:30 +0200

    Added Files

--- module/admini1.js
+++ module/admini1.js
@@ -4,47 +4,46 @@
  * 
  */
 
- module.exports = function (client) {
-    client.on('message', message => {
+module.exports = function (client) {
+  client.on('message', message => {
     // Mise en place des variables
-      var h = message.author.lastMessage.createdAt
-      var nom = message.author.username
+    var h = message.author.lastMessage.createdAt
+    var nom = message.author.username
 
-      if (message.content.startsWith('+inv')) {
-        message.channel.send('https://discordapp.com/oauth2/authorize?client_id=350995776131825664&scope=bot&permissions=2146958591')
-      }
+    if (message.content.startsWith('+inv')) {
+      message.channel.send('https://discordapp.com/oauth2/authorize?client_id=350995776131825664&scope=bot&permissions=2146958591')
+    }
 
-      if (message.content.startsWith('+kick')) {
-        var member = message.mentions.members.first()
-        let modRole = message.guild.roles.find('name', 'Mod')
-        if (message.member.roles.has(modRole.id)) {
-          member.kick().then((member) => {
+    if (message.content.startsWith('+kick')) {
+      var member = message.mentions.members.first()
+      let modRole = message.guild.roles.find('name', 'Mod')
+      if (message.member.roles.has(modRole.id)) {
+        member.kick().then((member) => {
           // Message réussis
-            message.channel.send(':wave: ' + member.displayName + ' à bien été **kick** :point_right: ')
-            console.log(h + ' +kick mis par: ' + nom)
-          }).catch(() => {
+          message.channel.send(':wave: ' + member.displayName + ' à bien été **kick** :point_right: ')
+          console.log(h + ' +kick mis par: ' + nom)
+        }).catch(() => {
           // Message du fail
-            message.channel.send('Acces refusé')
-            console.log(h + ' Tentative de "+kick" de: ' + nom)
-          })
-        }
+          message.channel.send('Acces refusé')
+          console.log(h + ' Tentative de "+kick" de: ' + nom)
+        })
       }
+    }
 
-      // BAN
+    // BAN
 
-      if (message.content.startsWith('+ban')) {
-        if (message.member.roles.has(modRole.id)) {
-          member.ban().then((member) => {
+    if (message.content.startsWith('+ban')) {
+      if (message.member.roles.has(modRole.id)) {
+        member.ban().then((member) => {
           // Message réussis
-            message.channel.send(':wave: ' + member.displayName + ' à bien été **ban** :point_right: ')
-            console.log(h + ' +kick mis par: ' + nom)
-          }).catch(() => {
+          message.channel.send(':wave: ' + member.displayName + ' à bien été **ban** :point_right: ')
+          console.log(h + ' +kick mis par: ' + nom)
+        }).catch(() => {
           // Message du fail
-            message.channel.send('Acces refusé')
-            console.log(h + ' Tentative de "+ban" de: ' + nom)
-          })
-        }
+          message.channel.send('Acces refusé')
+          console.log(h + ' Tentative de "+ban" de: ' + nom)
+        })
       }
-    })
-  }
+    }
+  })
 }
