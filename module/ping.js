e6726a8df8835c09dbce90219298d3498365274c
Unknown <babathiste@gmail.com>
2017-09-16 13:37:21 +0200

    Added Files

--- module/ping.js
+++ module/ping.js
@@ -4,33 +4,32 @@
  * 
  */
 
-    module.exports = function(client){
+module.exports = function (client) {
+  client.on('message', message => {
+    var nom = message.author.username
+    var h = message.author.lastMessage.createdAt
+    if (message.content === 'ping') {
+    // sa envoie pong
+      message.channel.send('pong')
+      console.log(h + ' "ping" mis par: ' + nom)
+    }
 
-        client.on('message', message =>{
-            var nom = message.author.username;
-            var h = message.author.lastMessage.createdAt;
-          if(message.content === 'ping') {
-	              // sa envoie pong
-            message.channel.send('pong');
-            console.log(h +' "ping" mis par: '+ nom);
-            }
+    if (message.content === 'Ping') {
+    // sa envoie C'est ping sans maj fdp
+      message.channel.send('C\'est ping sans maj fdp')
+      console.log(h + '" Ping" mis par: ' + nom)
+    }
 
-            if(message.content === 'Ping') {
-            	// sa envoie C'est ping sans maj fdp
-                message.channel.send('C\'est ping sans maj fdp');
-                console.log(h+'" Ping" mis par: '+ nom);
-            }
+    if (message.content === 'PONG') {
+    // sa envoie TG PONG
+      message.channel.send('TG PONG')
+      console.log(h + ' "PONG" mis par: ' + nom)
+    }
 
-            if(message.content === 'PONG') {
-            	// sa envoie TG PONG
-                message.channel.send('TG PONG');
-                console.log(h+' "PONG" mis par: '+ nom);
-            }
-
-            if(message.content === 'PING') {
-            	// sa envoie PONG
-                message.channel.send('PONG');
-                console.log(h+' "PING" mis par: '+ nom);
-            }
-
-        })}
+    if (message.content === 'PING') {
+    // sa envoie PONG
+      message.channel.send('PONG')
+      console.log(h + ' "PING" mis par: ' + nom)
+    }
+  })
+}
--- module/say.js
+++ module/say.js
@@ -3,19 +3,17 @@
  * @para {Client} client - The discord.js client.
  * 
  */
-const discord = require('discord.js');
-module.exports = function(client){
+const discord = require('discord.js')
+module.exports = function (client) {
+  client.on('message', message => {
+    if (message.content.startsWith('Mentionne moi')) {
+      var MessageAuthor = message.author.lastMessage.attachments.array(2)
+      message.reply(MessageAuthor)
+    }
 
-    client.on('message', message =>{
-
-        if(message.content.startsWith('Mentionne moi')){
-            var MessageAuthor = message.author.lastMessage.attachments.array(2);
-            message.reply(MessageAuthor);
-        }
-
-        if(message.content.startsWith('mentionne moi')){
-            var MessageAuthor = message.author.lastMessage.attachments.array(2);
-            message.reply(MessageAuthor);
-        }
-    })
+    if (message.content.startsWith('mentionne moi')) {
+      var MessageAuthor = message.author.lastMessage.attachments.array(2)
+      message.reply(MessageAuthor)
+    }
+  })
 }
