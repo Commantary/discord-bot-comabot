913c8c6f482270564eac270f350fddd2a7266930
Unknown <babathiste@gmail.com>
2017-09-17 12:18:47 +0200

    Added Files

--- module/ping.js
+++ module/ping.js
@@ -11,25 +11,35 @@ module.exports = function (client) {
     if (message.content === 'ping') {
     // sa envoie pong
       message.channel.send('pong')
-      console.log(h + ' "ping" mis par: ' + nom)
+      console.log(' "ping" mis par: ' + nom)
     }
 
     if (message.content === 'Ping') {
     // sa envoie C'est ping sans maj fdp
       message.channel.send('C\'est ping sans maj fdp')
-      console.log(h + '" Ping" mis par: ' + nom)
+      console.log('" Ping" mis par: ' + nom)
+    }
+
+    if (message.content === 'pong') {
+      // sa envoie ping
+      message.channel.send('Ping')
+    }
+
+    if (message.content === 'Pong') {
+      // sa envoie ping
+      message.channel.send('Ping')
     }
 
     if (message.content === 'PONG') {
     // sa envoie TG PONG
       message.channel.send('TG PONG')
-      console.log(h + ' "PONG" mis par: ' + nom)
+      console.log(' "PONG" mis par: ' + nom)
     }
 
     if (message.content === 'PING') {
     // sa envoie PONG
       message.channel.send('PONG')
-      console.log(h + ' "PING" mis par: ' + nom)
+      console.log(' "PING" mis par: ' + nom)
     }
   })
 }
