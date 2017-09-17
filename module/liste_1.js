4a23466d8edc52185cc5c2512cd9c98eb9a69e50
Unknown <babathiste@gmail.com>
2017-09-17 13:35:12 +0200

    Added Files

--- module/liste_1.js
+++ module/liste_1.js
@@ -15,7 +15,7 @@ module.exports = function (client) {
       return msg.channel.sendMessage('', {embed: {
         title: 'Stats:',
         color: 0x0AD90A,
-        description: 'Version: __**' + version + '**__       Nom du bot: __**ComaBot**__',
+        description: '\nVersion: __**' + version + '**__       Nom du bot: __**ComaBot**__\n',
         footer: {
           icon_url: 'https://cdn.discordapp.com/avatars/350995776131825664/d6348248148cb6acd73f2d391c12c9fe.png?size=2048',
           text: 'Message par ComaBot.'
