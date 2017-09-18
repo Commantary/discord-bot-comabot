9f8934a4f49176e4d0979a4c000aba4d59e533aa
Unknown <babathiste@gmail.com>
2017-09-18 21:19:29 +0200

    Added Files

--- module/liste_1.js
+++ module/liste_1.js
@@ -12,12 +12,11 @@ module.exports = function (client) {
     var name = setting.name
     var msg = message
     var icone = setting.icon_creator
-    var iconeComabot = 'https://cdn.discordapp.com/avatars/350995776131825664/d6348248148cb6acd73f2d391c12c9fe.png?size=2048'
 
     if (message.content === prefix + 'stats') {
       return msg.channel.sendMessage('', {embed: {
+        icon_url: 'https://cdn.discordapp.com/avatars/350995776131825664/d6348248148cb6acd73f2d391c12c9fe.png?size=2048',
         title: 'ComaBot',
-        icon_url: iconeComabot,
         color: 0x0AD90A,
         description: '\n\nVersion: __**' + version + '**__       Nom du bot: __**' + name + '**__\n\n',
         footer: {
