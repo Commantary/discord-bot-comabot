a0cf448377727c16e9c7e78c5d81d0d181164816
Unknown <babathiste@gmail.com>
2017-09-20 21:26:11 +0200

    Added Files

--- module/liste_2.js
+++ module/liste_2.js
@@ -16,26 +16,25 @@ module.exports = function (client) {
     if (message.content === 'Avatar ComaBot') {
       message.channel.send('C\'est quoi mon avatar')
     }
-
-    // ARRIVE
-    client.on('guildMemberAdd', member => {
+  })
+  // ARRIVE
+  client.on('guildMemberAdd', member => {
     // Envoie un message au channel designer :
-      const channel = member.guild.channels.find('name', 'bonjour-bye')
-      // Fais rien si le channel existe pas
-      if (!channel) return
-      // Envoie le message, en mentionnant le membre
-      let role = message.guild.roles.find('name', 'Nouveaux retard')
-      channel.send(`-----------------------------------\n   Bienvenue sur le serveur ${member}\n-----------------------------------`)
-      message.guild.member({member}).addRole(role.id)
-    })
-    // DEPART
-    client.on('guildMemberRemove', member => {
+    const channel = member.guild.channels.find('name', 'bonjour-bye')
+    // Fais rien si le channel existe pas
+    if (!channel) return
+    // Envoie le message, en mentionnant le membre
+    let role = message.guild.roles.find('name', 'Nouveaux retard')
+    channel.send(`-----------------------------------\n   Bienvenue sur le serveur ${member}\n-----------------------------------`)
+    message.guild.member({member}).addRole(role.id)
+  })
+  // DEPART
+  client.on('guildMemberRemove', member => {
     // Envoie un message au channel designer: 
-      const channel = member.guild.channels.find('name', 'bonjour-bye')
-      // Fais rien si le channel existe pas
-      if (!channel) return
-      // Envoie le message, en mentionnant le membre
-      channel.send(`-----------------------------------\n   ${member} est parti du serveur  \n-----------------------------------`)
-    })
+    const channel = member.guild.channels.find('name', 'bonjour-bye')
+    // Fais rien si le channel existe pas
+    if (!channel) return
+    // Envoie le message, en mentionnant le membre
+    channel.send(`-----------------------------------\n   ${member} est parti du serveur  \n-----------------------------------`)
   })
 }
