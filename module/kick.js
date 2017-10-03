2f6e7a99a0d1153b033d36a6d89b770d9cce6c16
Unknown <babathiste@gmail.com>
2017-10-03 11:05:25 +0200

    Added Files

--- module/kick.js
+++ module/kick.js
@@ -1,24 +1,28 @@
 
 module.exports.run = async (client, message, args) => {
-// Mise en place des variables
-  var member = message.guild.member(message.mentions.members.first())
-  // var member = message.guild.member(message.mentions.members.first())
-  let modRole = message.guild.roles.find('name', 'Mod')
-  // Si il y a pas de mention dans la commande
-  if (!member) {
-    message.channel.send('`+kick [@(le joueur a kick)]`')
-  } else {
-    // If du modRole
-    if (message.member.roles.has(modRole.id)) {
-      let kickMember = message.guild.member(message.mentions.users.first())
-      let reason = args.slice(1).join(' ')
-      message.guild.member(kickMember).kick(reason)
-      // Message réussis
-      message.channel.send(':wave: **' + kickMember.displayName + '** à été __**kick**__ par: **' + message.author.username + '**')
-      console.log(h + ' ' + prefix + 'kick mis par : ' + nom)
-      // Message du fail
+  // Mise en place des variables
+    const fs = require('fs')
+    const setting = JSON.parse(fs.readFileSync('./module/config.json', 'utf8'))
+    const prefix = setting.prefix
+    var member = message.guild.member(message.mentions.members.first())
+    // var member = message.guild.member(message.mentions.members.first())
+    let modRole = message.guild.roles.find('name', 'Mod')
+    // Si il y a pas de mention dans la commande
+    if (!member) {
+      message.channel.send('`+kick [@(le joueur a kick)]`')
     } else {
-      return message.reply('Acces refusé')
+      // If du modRole
+      if (message.member.roles.has(modRole.id)) {
+        let kickMember = message.guild.member(message.mentions.users.first())
+        let reason = args.slice(1).join(' ')
+        message.guild.member(kickMember).kick(reason)
+        // Message réussis
+        message.channel.send(':wave: **' + kickMember.displayName + '** à été __**kick**__ par: **' + message.author.username + '**')
+        console.log(prefix + 'kick mis par : ' + message.author.username)
+        // Message du fail
+      } else {
+        return message.reply('Acces refusé')
+      }
     }
   }
-}
+  
--- module/test_fs.js
+++ module/test_fs.js
@@ -1,9 +1,5 @@
-/**
- * 
- * @para {Client} client - The discord.js client.
- * 
- */
 
+<<<<<<< Updated upstream
 module.exports = function (client) {
   client.on('message', message => {
     // MISE EN PLACE DES VARIABLES
@@ -17,15 +13,32 @@ module.exports = function (client) {
       let newPrefix = args.slice('prefix').join(' ')
       // var contenuD = fs.readFile('localbot/module/teste.json', 'UTF-8')
       var contenu = JSON.parse(fs.readFileSync('./module/config.json', 'utf8'))
+=======
+module.exports.run = async (client, message, args) => {
+  // MISE EN PLACE DES VARIABLES
+  var fs = require('fs')
+  const setting = JSON.parse(fs.readFileSync('./module/config.json', 'utf8'))
+  let modRole = message.guild.roles.find('name', 'Mod')
+  // FIN DES VARIABLES
+>>>>>>> Stashed changes
 
-      contenu.prefix = newPrefix
+  // DEBUT DU CODE DE LA COMMANDE
+  if (message.member.roles.has(modRole.id)) {
+    let newPrefix = args.slice('prefix').join(' ')
+    // var contenuD = fs.readFile('localbot/module/teste.json', 'UTF-8')
+    var contenu = JSON.parse(fs.readFileSync('./module/config.json', 'utf8'))
 
-      fs.writeFile('./module/config.json', JSON.stringify(contenu), (err) => {
-        if (err) console.error(err)
-      })
+    contenu.prefix = newPrefix
 
-      message.channel.send('Le nouveau prefix est: ' + contenu.prefix)
-    }
+    fs.writeFile('./module/config.json', JSON.stringify(contenu), (err) => {
+      if (err) console.error(err)
+    })
+
+    message.channel.send('Le nouveau prefix est: ' + contenu.prefix)
+  } else {
+    message.channel.send({embed: {
+      color: 16711680,
+      description: 'Tu n\'as pas les autorisations pour faire sa !'
+    }})
   }
-  )
 }
