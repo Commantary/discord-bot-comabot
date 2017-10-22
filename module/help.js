f9d62c1e770c1ce40c2d54f4f5b3bae8ada55049
Unknown <babathiste@gmail.com>
2017-10-22 17:54:57 +0200

    Added Files

--- module/help.js
+++ module/help.js
@@ -10,7 +10,7 @@ module.exports.run = async (client, message, args) => {
       description: ':tools: **COMMANDES** :tools: \n\nCommandes Admin: +help admin \n\nCommandes Fun: +help fun',
       footer: {
         icon_url: iconUrlLien,
-        text: 'Message par ComaBot.'
+        text: 'Créer par Commentary.'
       }
     }}).catch(console.error)
       .catch(function () {
@@ -21,10 +21,10 @@ module.exports.run = async (client, message, args) => {
     return message.channel.sendMessage('', {embed: {
       title: 'Commandes:',
       color: 0xff0000,
-      description: ':tools: **Liste des commandes** :tools: \n\n__+ban @UnGars__ | Pour ban un membre en ayant le rôle **Mod**\n\n__+kick @UnGars__ | Pour kick un membre en ayant le rôle **Mod**\n\n__+mute @jeanKevin__ | Pour mute une personne en texte\n\n__+unmute @jeanKevin__ | Pour unmute une personne en texte\n\n__+vmute @UnNoob__ | Pour mute serveur un membre\n\n__+vunmute @UnNoob__ | Pour demute serveur un membre\n\nD\'autre commandes sont en dévelopement...',
+      description: ':tools: **Liste des commandes** :tools: \n\n**Les commandes nécessite le role** __**MOD**__ !\n\n__+ban @ComaBot__ | Pour ban un membre (**non stable**)\n\n__+kick @ComaBot__ | Pour kick un membre (**non stable**)\n\n__+mute @ComaBot__ | Pour mute une personne en texte (**non stable**)\n\n__+unmute @ComaBot__ | Pour unmute une personne en texte (**non stable**)\n\n__+vmute @ComaBot__ | Pour mute serveur un membre\n\n__+vunmute @ComaBot__ | Pour demute serveur un membre\n\nD\'autre commandes sont en dévelopement...',
       footer: {
         icon_url: 'https://cdn.discordapp.com/avatars/350995776131825664/d6348248148cb6acd73f2d391c12c9fe.png?size=2048',
-        text: 'Message par ComaBot.'
+        text: 'Créer par Commentary.'
       }
     }}).catch(console.log(h + ' "help admin" mis par: ' + message.author.username))
       .catch(function () {
@@ -36,10 +36,10 @@ module.exports.run = async (client, message, args) => {
     return message.channel.sendMessage('', {embed: {
       title: 'Commandes:',
       color: 0xff0000,
-      description: ':tools: **Liste des commandes** :tools: \n\n**+avatar** : Cela te permet d\'avoir l\'url d\'un avatar discord\n\n**+ping** : <mystere> \n\n**+choice <choix1> <choix2>** : Pour proposer un choix au bot\n\n**+cal <chiffre> <signe> <chiffre>** : Pour faire un calcule rapide\n\n**+cat** : Pour afficher une image de chat random\n\n',
+      description: ':tools: **Liste des commandes** :tools: \n\n__+avatar @ComaBot__ : Cela te permet d\'avoir l\'url d\'un avatar discord\n\n__+ping__ : Pour vérifié si le bot est fonctionnel\n\n__+choice <choix1> <choix2>__ : Pour proposer un choix au bot (Pour les choix vous ne pouvez mettre que un mot)\n\n__+cal <____**chiffre**____> <signe> <chiffre / nombre>__ : Pour faire un calcule rapide\n\n__+cat__ : Pour afficher une image de chat **mignon** random\n\n',
       footer: {
         icon_url: 'https://cdn.discordapp.com/avatars/350995776131825664/d6348248148cb6acd73f2d391c12c9fe.png?size=2048',
-        text: 'Message par ComaBot.'
+        text: 'Créer par Commentary.'
       }
     }}).catch(console.log(h + ' "help fun" mis par: ' + message.author.username))
       .catch(function () {
