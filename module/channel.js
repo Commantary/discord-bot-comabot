6f53bfbe8a9151693af058764926b479c49fb55b
Unknown <babathiste@gmail.com>
2017-10-23 15:47:39 +0200

    Added Files

--- module/channel.js
+++ module/channel.js
@@ -5,19 +5,30 @@ module.exports.run = async (client, message, args) => {
   var channel = args.slice('channel').join(' ')
   var channelId = message.guild.channels.find('name', channel).id
   var serveur = message.guild.id
+
+  var dt = process.env.JSONAPI || process.argv[2]
+  if (!dt) {
+  console.log('L\'url n\'existe pas!')
+  }
+
   var url = 'https://api.myjson.com/bins/1eurgn'
 
   function callback(err, response, body) {
     if (err) {
+      console.log(err)
+    }
+    var channelNameSearch = message.guild.channels.find('name', channel)
+    if(!channelNameSearch){ // ON VERIFIE SI LE CHANNEL EXISTE
+      message.channel.send({embed: {
+        color: 11797508,
+        description: 'Le channel **' + channel + '** n\'existe pas !'
+      }})
+    } else {
       message.channel.send({embed: {
         color: 10790566,
-        description: 'Erreur: ' + err
+        description: 'Le nouveau channel <#' + channelId + '> à bien été mis !'
       }})
     }
-    message.channel.send({embed: {
-      color: 10790566,
-      description: 'Le nouveau channel " **' + channelId + '** "à bien été mis !'
-    }})
   }
 
   // On get le json
