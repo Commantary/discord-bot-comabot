ff987ef3e404c55131d2fbfbc341a4eb6de1f996
Unknown <babathiste@gmail.com>
2017-10-23 16:12:12 +0200

    Added Files

--- module/channel.js
+++ module/channel.js
@@ -5,11 +5,7 @@ module.exports.run = async (client, message, args) => {
   var channel = args.slice('channel').join(' ')
   var channelId = message.guild.channels.find('name', channel).id
   var serveur = message.guild.id
-
-  var url = process.env.JSONAPI || process.argv[2]
-  if (!url) {
-  console.log('L\'url n\'existe pas!')
-} else {
+  var url = 'https://api.myjson.com/bins/1eurgn'
 
   function callback(err, response, body) {
     if (err) {
@@ -29,23 +25,38 @@ module.exports.run = async (client, message, args) => {
     }
   }
 
-  // On get le json
-  request(url, (err, res, body) => {
-    // si sa arrive la on log
-    console.log('Tentation de chargement')
-    // Si y'a une erreur
-    if(err || res.statusCode!== 200)return
-    // Si y'en a pas
-    console.log('chargé avec succés')
-    var objet = JSON.parse(body)
-    objet[serveur] = {
-      channel: channelId
+  let modRole = message.guild.roles.find('name', 'Mod')
+  if(!modRole){
+    message.channel.send({embed: {
+      color: 11797508,
+      description: 'Le role **Mod** n\'existe pas !'
+    }})
+  } else {
+    if (message.member.roles.has(modRole.id)) {
+      // On get le json
+      request(url, (err, res, body) => {
+        // si sa arrive la on log
+        console.log('Tentation de chargement')
+        // Si y'a une erreur
+        if(err || res.statusCode!== 200)return
+        // Si y'en a pas
+        console.log('chargé avec succés')
+        var objet = JSON.parse(body)
+        objet[serveur] = {
+          channel: channelId
+        }
+
+        // On put tout sa!
+        request({ url: url, method: 'PUT', json: objet}, callback)
+      })
+    } else {
+      message.channel.send({embed: {
+        color: 11797508,
+        description: 'Tu \'as pas le role pour faire cela !'
+      }})
     }
-
-    // On put tout sa!
-    request({ url: url, method: 'PUT', json: objet}, callback)
-  })
-}
+  }
 
 
-}
+
+} // FIN DU MODULE EXPORTS
