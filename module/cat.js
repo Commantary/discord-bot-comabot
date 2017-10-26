eca6cf5f5e95eb853a27995544a81b5918af4e9f
Unknown <babathiste@gmail.com>
2017-10-26 10:32:25 +0200

    Mise à jour de la commande `+cat`

--- module/cat.js
+++ module/cat.js
@@ -1,28 +1,17 @@
-const api = 'https://jsonbin.io/b/59e136d207d53e275ddfed88'
-const fetch = require('snekfetch')
+const request = require('request')
+const fs = require('fs')
+const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
 
-var randomCat = require('random-cat')
- 
-// Random cat picture of various widths and heights 
-// Possible values: 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600 
-// http://lorempixel.com/100/600/cats 
-var url = randomCat.get()
-// Possible values: abstract, animals, business, cats, city, food, nightlife, fashion, people, nature, sports, technics, transport 
-// Random cat picture of specific size 
-// http://lorempixel.com/120/600/cats 
-var urlWithSize = randomCat.get({
-  width: 720,
-  height: 480
-})
+	request.get('http://random.cat/meow', function (err, res, body) {
+		var obj = JSON.parse(body)
 
-let msg = await message.channel.send({embed: {
-	description: 'Génération du chat....'
-	}})
-await message.channel.sendFile(url, 'cat.jpg')
+		const embed = new Discord.RichEmbed()
+			.setImage(obj.file)
+			.setColor(12434877)
+			.setFooter('Créer par Commentary', client.user.avatarURL)
+			message.channel.send({embed})
+	}) // FIN DU REQUEST
 
-
-msg.delete()
-
-}
+} // FIN DU MODULE EXPORTS
