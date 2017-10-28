214d50f307b2e640b2b8fdc4adbdf06d0d509428
Commantary <31055935+Commantary@users.noreply.github.com>
2017-10-28 13:24:20 +0200

    Commande `+kick` OP !

--- module/kick.js
+++ module/kick.js
@@ -1,28 +1,44 @@
 
 module.exports.run = async (client, message, args) => {
-  // Mise en place des variables
-    const fs = require('fs')
-    const setting = JSON.parse(fs.readFileSync('./module/config.json', 'utf8'))
-    const prefix = setting.prefix
-    var member = message.guild.member(message.mentions.members.first())
-    // var member = message.guild.member(message.mentions.members.first())
-    let modRole = message.guild.roles.find('name', 'Mod')
-    // Si il y a pas de mention dans la commande
-    if (!member) {
-      message.channel.send('`+kick [@(le joueur a kick)]`')
-    } else {
-      // If du modRole
-      if (message.member.roles.has(modRole.id)) {
-        let kickMember = message.guild.member(message.mentions.users.first())
-        let reason = args.slice(1).join(' ')
-        message.guild.member(kickMember).kick(reason)
-        // Message réussis
-        message.channel.send(':wave: **' + kickMember.displayName + '** à été __**kick**__ par: **' + message.author.username + '**')
-        console.log(prefix + 'kick mis par : ' + message.author.username)
-        // Message du fail
-      } else {
-        return message.reply('Acces refusé')
-      }
-    }
-  }
-  
+
+    if(!message.member.roles.some(r=>["Administrator", "Mod", "Modo", "mod", "Admin"].includes(r.name)) )
+      return message.channel.send({embed: {
+      	color: 0xff0000,
+      	description: "Désolé tu n'as pas les permissions pour !"
+      }})
+
+
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.channel.send({embed: {
+      	color: 0xff0000,
+      	description: "Mentionne une personne sur ce serveur!"
+      }})
+
+    if(!member.kickable)
+      return message.channel.send({embed: {
+      	color: 0xff0000,
+      	description: "Je ne peux pas kick cette personne!\nIl a un plus haut role?\nJ'ai les permissions pour kick?"
+      }})
+
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.channel.send({embed: {
+      	color: 0xff0000,
+      	description: "Il faut dire une raison pour kick!"
+      }})
+
+    await member.kick(reason)
+
+      .catch(error => message.channel.send({embed: {
+      	color: 0xff0000,
+      	description: `Désolé ${message.author} Je ne peux pas kick car : ${error}`
+      }}))
+    message.reply({embed: {
+      	color: 0xff0000,
+      	description: `<@${member.user.tag}> a été kick par <@${message.author.tag}> pour: **${reason}`
+      }})
+
+
+}
