dae96ad2856fb093774041cda321745da42cbcd4
Unknown <babathiste@gmail.com>
2017-10-28 14:56:40 +0200

    +avatar mise à jour !

--- module/avatar.js
+++ module/avatar.js
@@ -9,12 +9,13 @@ module.exports.run = async (client, message, args) => {
     // ON APPELLER LA VARIABLE MEMBER ET ON CREER L'ARRAY USER POUR MEMBER ET ON RECUPERE L'AVATAR URL
     let msg = await message.channel.send('Génération de l\'avatar...')
 
-    await message.channel.send({files: [
-      {
-        attachment: member.displayAvatarURL,
-        name: 'avatar.png'
-      }
-    ]})
+
+    const embed = new Discord.RichEmbed()
+      .setImage(member.displayAvatarURL)
+      .setColor(12434877)
+      .setFooter('Créer par Commentary', client.user.avatarURL)
+
+    await message.channel.send({embed})
 
     msg.delete()
     // message.channel.send('Voici son avatar: ' + member.user.avatarURL + '')
