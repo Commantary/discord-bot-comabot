ef41a0152dc22e15e70c9c7cfebdf3d9b4001d8a
Unknown <babathiste@gmail.com>
2017-10-28 14:55:03 +0200

    `+unban` fonctionnel !

--- module/unban.js
+++ module/unban.js
@@ -1,29 +1,59 @@
 
 module.exports.run = async (client, message, args) => {
-  if (message.author.id === '214846601066315776') {
-    // Mise en place des variables
-    var member = message.guild.member(message.mentions.members.first())
-    let modRole = message.guild.roles.find('name', 'Mod')
-    let unbanMember = message.guild.member(message.mentions.users.first())
-    // Si il y a pas de mention dans la commande
-    if (!member) {
-      message.channel.send('Mentionne une personne à unban !')
-    }
-    // If du modRole
-    if (message.member.roles.has(modRole.id)) {
-      let reason = args.slice(1).join(' ')
-      message.guild.unban(unbanMember)
-      // Message réussis
-      message.channel.send(':wave: **** à été __**unban**__ par: **' + message.author.username + '**')
-      console.log(h + ' +unban mis par: ' + nom)
-      // Message du fail
-    } else {
-      message.channel.send('Acces refusé')
-    }
-  } else {
-    message.channel.send({embed: {
-      color: 16711680,
-      description: 'La commande est en developpement...'
+  // Mise en place des variables
+    let reason = args.slice(1).join(' ')
+    let member = args[0]
+    let channelLogs = message.guild.channels.find('name', 'logs')
+
+    if(reason.length < 1) return message.channel.send({embed: {
+      color: 8583768,
+      description: 'Il faut dire une raison pour unban.'
     }})
+
+
+    if(!member) return message.channel.send({embed: {
+      color: 8583768,
+      description: 'Vous devez mettre un ID de membre ban.'.catch(console.error)
+    }})
+
+    await message.guild.unban(member, 2)
+    if(!channelLogs){
+      message.channel.send({embed: {
+        color: 8583768,
+        description: `<@${member}> a été unban par <@${message.author.id}> pour: **${reason}**`
+      }})
+    } else {
+      channelLogs.send({embed: {
+        color: 122789,
+        fields: [
+        {
+          name: 'Action',
+          value: 'unban',
+          inline: false
+        },
+        {
+          name: 'Membre unban',
+          value: member,
+          inline: true
+        },
+        {
+          name: 'Par',
+          value: message.author.tag,
+          inline: true
+        },
+        {
+          name: 'Raison',
+          value: reason,
+          inline: true
+        }
+        ]
+      }})
+
+      message.channel.send({embed: {
+        color: 8583768,
+        description: `<@${member}> a été unban par <@${message.author.id}> pour: **${reason}**`
+      }})
+
+
+    }
   }
-}
