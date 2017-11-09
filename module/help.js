61a9f1e5082798f7405f4b68aaa3805e9d76c7c5
Commantary <31055935+Commantary@users.noreply.github.com>
2017-11-09 14:16:25 +0100

    Rajout du +help sis

--- module/help.js
+++ module/help.js
@@ -5,10 +5,10 @@ module.exports.run = async (client, message, args) => {
     message.channel.send({embed: {
         title: "Commandes:",
         color: 0xff0000,
-        description: ':tools: **COMMANDES** :tools: \n\nCommandes Admin: +help admin \n\nCommandes Fun: +help fun',
+        description: ':tools: **COMMANDES** :tools: \n\nCommandes Admin: +help admin \n\nCommandes Fun: +help fun\n\nCommandes SiS: +help sis',
         footer: {
           icon_url: client.user.avatarURL,
-          text: 'Créer par Commentary.'
+          text: 'Créer par Commentary'
         }
     }}).catch(console.error)
   } // FIN DU IF DU +HELP NORMAL
@@ -20,7 +20,7 @@ module.exports.run = async (client, message, args) => {
             description: ':tools: **Liste des commandes** :tools: \n\n**Les commandes nécessite le role** __**MOD**__ !\n\n__+ban @ComaBot__ | Pour ban un membre\n\n__+unban <id du membre>__ | Pour unban un membre\n\n__+kick @ComaBot__ | Pour kick un membre\n\n__+mute @ComaBot__ | Pour mute une personne en texte (**non stable**)\n\n__+unmute @ComaBot__ | Pour unmute une personne en texte (**non stable**)\n\n__+vmute @ComaBot__ | Pour mute serveur un membre\n\n__+vunmute @ComaBot__ | Pour demute serveur un membre\n\n__+channel <channel>__ | Pour choisir le channel ou le bot diras les arriver et départs\n\nD\'autres commandes sont en dévelopement...',
             footer: {
               icon_url: client.user.avatarURL,
-              text: 'Créer par Commentary.'
+              text: 'Créer par Commentary'
             }
         }}).catch(console.error)
     } // FIN DU IF DU +HELP ADMIN
@@ -32,7 +32,19 @@ module.exports.run = async (client, message, args) => {
               description: ':tools: **Liste des commandes** :tools: \n\n**+avatar** : Cela te permet d\'avoir l\'url d\'un avatar discord\n\n**+ping** : <mystere> \n\n**+choice <choix1> <choix2>** : Pour proposer un choix au bot\n\n**+cal <chiffre> <signe> <chiffre>** : Pour faire un calcule rapide\n\n**+cat** : Pour afficher une image de chat random\n\n**+dog** | Pour afficher une image de chien random\n\n',
               footer: {
               icon_url: client.user.avatarURL,
-              text: 'Créer par Commentary.'
+              text: 'Créer par Commentary'
+            }
+          }}).catch(console.error)
+      } // FIN DU IF DU +HELP FUN
+
+      if(message.content === '+help sis'){ // IF DU +HELP FUN
+          message.channel.send({embed: {
+              title: 'Commandes:',
+              color: 0xff0000,
+              description: ':tools: **Liste des commandes** :tools: \n\n**+sis <votre message>** :  vous permet de mettre un message dans tous les serveurs ou est ComaBot qui sont appeller `sis`\n\nD\'autres commandes sont en dévelopement...',
+              footer: {
+              icon_url: client.user.avatarURL,
+              text: 'Créer par Commentary'
             }
           }}).catch(console.error)
       } // FIN DU IF DU +HELP FUN
